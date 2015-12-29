91a3090661f0f582a87a579abd936c283752294a
Nabil Abouchouayb <faouzzz@gmail.com>
2015-12-29 15:13:22 +0100

    save domain to user model on create from ad

--- lib/devise_active_directory_authenticatable/models/ad_user.rb
+++ lib/devise_active_directory_authenticatable/models/ad_user.rb
@@ -60,6 +60,7 @@ module Devise
           # Check to see if we have the same user
           unless user.nil?
             return :no_email unless user.email.present?
+            user.domain = domain
             user.save if user.new_record? and ::Devise.ad_create_user
             user.login if user.respond_to?(:login)
             return user
